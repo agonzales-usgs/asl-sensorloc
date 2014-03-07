40ed750e64fe3dd476b44ef5e3c08fbd808a1d7f
agonzales-usgs <agonzales@usgs.gov>
2014-03-07 14:09:17 -0700

    testing sensorloc stream class

--- test_sensorloc.py
+++ test_sensorloc.py
@@ -3,10 +3,14 @@
 #third party imports
 from sensorloc import streams
 from obspy import UTCDateTime
+from obspy import core
+import time
+import os.path 
 
 #stdlib imports
 import os.path
 
+'''
 def test_getTimeExtent():
     seedstart = UTCDateTime('2010-04-15T00:00:00.023145Z')
     seedend = UTCDateTime('2010-04-15T23:59:59.960645Z')
@@ -31,9 +35,24 @@ def test_trim():
     myrange = mystreams.getTimeExtent()
     assert myrange['start'] == seedstart
     assert myrange['end'] == seedend
-
-
-
-
-    
-    
+'''
+sl1 = streams.Streams()
+sl2 = streams.Streams()
+data1 = core.stream.Stream()
+data2 = core.stream.Stream()
+seed1 = '/home/agonzales/Documents/asl-sensorloc/00_LHZ.512.seed'
+seed2 = '/home/agonzales/Documents/telemetry_days/IU_ANMO/2014/2014_062/00_LHZ.512.seed'
+sl1.addFile(seed1)
+sl2.addFile(seed2)
+print
+print "sl1 = " + str(sl1)
+print "sl2 = " + str(sl2)
+print
+strm1 = core.read(seed1)
+strm2 = core.read(seed2)
+print "strm1() xs0 = " + str(strm1);	# test without += stream
+print "strm2() tr1 = " + str(strm2);	
+data1 += strm1 # add stream to data list
+data2 += strm2
+print "data1 xs0 = " + str(data1)
+print "data2 tr1 = " + str(data2)
